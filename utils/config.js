759b3b508bddf54ddbc934382e6ac21221808d7d
ReedGoldsworthy <reedgoldsworthy2@gmail.com>
2024-08-20 15:59:05 -0500

    demo build 1.0 - had to change localhost in frontend

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 let MONGODB_URI = process.env.MONGODB_URI;
-let PORT = process.env.PORT || 3001;
+let PORT = process.env.PORT || 3002;
 let CLIENT_ID = process.env.CLIENT_ID;
 let CLIENT_SECRET = process.env.CLIENT_SECRET;
 let ACCESS_TOKEN = null;
